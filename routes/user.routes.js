2c837f7a5c8106bb37b508a5ebae42d365173fb0
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-07-22 16:47:20 +0200

    Add /profile and /profile/settings endpoint to the user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,12 +2,30 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/logged', (req, res) => {
-  res.render('logged');
+  const loggedUser = req.user;
+  if (loggedUser){
+    res.render('logged');
+  } else {
+    res.render('noPermission');
+  }
 });
 
-router.get('/no-permission', (req, res) => {
-  res.render('noPermission');
+router.get('/profile', (req, res) => {
+  const loggedUser = req.user;
+  if (loggedUser){
+    res.render('userProfile');
+  } else {
+    res.render('noPermission');
+  }
 });
 
+router.get('/profile/settings', (req, res) => {
+  const loggedUser = req.user;
+  if (loggedUser){
+    res.render('profileSettings');
+  } else {
+    res.render('noPermission');
+  }
+});
 
 module.exports = router;
