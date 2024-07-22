aa0480e38d3f376e1cd467cc9a92184633245755
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-07-22 16:15:19 +0200

    Add separating endpoints into separate files

--- server.js
+++ server.js
@@ -4,17 +4,22 @@ const path = require('path');
 const hbs = require('express-handlebars');
 const passport = require('passport');
 const session = require('express-session');
-const passportConfig = require('./config/passport');
+const passportSetup = require('./config/passport');
 
 const app = express();
 
-app.use(session({ secret: 'anything' }));
-app.use(passport.initialize());
-app.use(passport.session());
-
+// set handlebars as view engine
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
+// init session mechanism
+app.use(session({ secret: 'anything' }));
+
+// init passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// standard middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,21 +29,8 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['email', 'profile'] }));
-
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
-  (req, res) => {
-    res.redirect('/user/logged');
-  }
-);
-app.get('/user/logged', (req, res) => {
-  res.render('logged');
-});
-
-app.get('/user/no-permission', (req, res) => {
-  res.render('noPermission');
-});
+app.use('/auth', require('./routes/auth.routes'));
+app.use('/user', require('./routes/user.routes'));
 
 app.use('/', (req, res) => {
   res.status(404).render('notFound');
